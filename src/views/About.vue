891c1e242e6dde6dd8c60633168eada700f99884
Lektor <denislour@gmail.com>
2021-10-16 00:59:24 +0700

    Initial Clavor project.

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        About {{ name }}
+    </div>
+</template>
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const name = computed(() => {
+    return store.state.user.name;
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <Navbar />
+    <div class="h-screen flex flex-row flex-wrap">
+        <Sidebar />
+    </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+
+import Navbar from '../components/Navbar.vue';
+import Sidebar from '../components/Sidebar.vue';
+
+const store = useStore();
+const router = useRouter();
+const name = computed(() => {
+    return store.state.user.name;
+});
+const newName = ref('');
+
+function saveName() {
+    store.dispatch('saveName', newName.value);
+    newName.value = '';
+    router.push('/about');
+}
+</script>
